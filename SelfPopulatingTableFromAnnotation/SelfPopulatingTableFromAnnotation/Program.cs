6468ca951f2776845e7fa73cab53ab5e171b2256
MDCCXIII <MDCCXIII@MDCCXIII-PC>
2017-02-22 00:56:39 -0600

    Removed unnecessary usings

--- SelfPopulatingTableFromAnnotation/SelfPopulatingTableFromAnnotation/Program.cs
+++ SelfPopulatingTableFromAnnotation/SelfPopulatingTableFromAnnotation/Program.cs
@@ -1,9 +1,4 @@
 ﻿using SelfPopulatingTableFromAnnotation.Sql_Adapter;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SelfPopulatingTableFromAnnotation {
     class Program {
