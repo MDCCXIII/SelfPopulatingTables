2d408697538f7aeb0bf00b87dbc090d72f412601
MDCCXIII <MDCCXIII@MDCCXIII-PC>
2017-02-26 21:56:01 -0600

    Added T4 code generation example

---
+++ ConsoleApplication2/ConsoleApplication2/Controls.cs
@@ -0,0 +1,11 @@
+﻿namespace T4_cs_generation_from_Model {
+	public static class Controls {
+		public const string control1 = "control1";
+		public const string control2 = "control2";
+		public const string mycustomcontrol = "mycustomcontrol";
+		public const string IsCaseSensitive = "IsCaseSensitive";
+		public const string addedcontrol1 = "addedcontrol1";
+		public const string addedControl2 = "addedControl2";
+		public const string Kaitlyne = "Kaitlyne";
+	}
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace T4_cs_generation_from_Model {
+    class Program {
+        static void Main(string[] args) {
+            
+        }
+    }
+}
